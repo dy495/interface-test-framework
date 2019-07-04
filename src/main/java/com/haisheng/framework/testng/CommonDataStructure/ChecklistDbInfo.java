3533594e67c0396f1e09cae633c78333eede7eec
liaoxiangru <1400893423@qq.com>
2019-07-04 13:04:00 +0800

    add appId

--- src/main/java/com/haisheng/framework/testng/CommonDataStructure/ChecklistDbInfo.java
+++ src/main/java/com/haisheng/framework/testng/CommonDataStructure/ChecklistDbInfo.java
@@ -5,15 +5,17 @@ public class ChecklistDbInfo {
     public static final int DB_APP_ID_EDGE_SERVICE = 1;
     public static final int DB_APP_ID_CLOUD_SERVICE = 2; //云端服务
     public static final int DB_APP_ID_MANAGE_PORTAL_SERVICE = 3; //管理后台
-    public static final int DB_APP_ID_AD_SERVICE = 4; //开放平台
+    public static final int DB_APP_ID_OPEN_PLATFORM_SERVICE = 4; //开放平台
     public static final int DB_APP_ID_SCREEN_SERVICE = 5; //大屏展示
     public static final int DB_APP_ID_SHELF_SERVICE = 6; //货架商品
+
     public static final int DB_SERVICE_ID_EDGE_SERVICE = 1;
     public static final int DB_SERVICE_ID_CUSTOMER_DATA_SERVICE = 2;
     public static final int DB_SERVICE_ID_BODY_SERVICE = 3;
     public static final int DB_SERVICE_ID_FACE_SERVICE = 4;
     public static final int DB_SERVICE_ID_AD_SERVICE = 5;
     public static final int DB_SERVICE_ID_SHELF_SERVICE = 7;
+    public static final int DB_SERVICE_ID_CONTROL_CENTER_SERVICE = 8;
 
     public static final String QA_SEP = "{qa-var-qa}";
 
