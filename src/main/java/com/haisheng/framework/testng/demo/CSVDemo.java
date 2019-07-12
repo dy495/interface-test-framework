4ddeca740eaa54bd73705f203e1af860afeedd8f
yuhaisheng <yuhaisheng@winsense.ai>
2019-07-12 15:12:42 +0800

    update: update csv filed content

--- src/main/java/com/haisheng/framework/testng/demo/CSVDemo.java
+++ src/main/java/com/haisheng/framework/testng/demo/CSVDemo.java
@@ -15,12 +15,16 @@ public class CSVDemo {
     }
 
     @Test(dataProvider = "CsvDataProvider", dataProviderClass = CsvDataProvider.class)
-    public void yuhaisheng_demo2(String id, String u1, String u2, String u3) {
+    public void yuhaisheng_demo2(String id, String u1, String u2, String u3, String u4, String u5, String u6, String u7) {
 
         System.out.println("id: " + id);
         System.out.println("u1: [" + u1 + "]");
         System.out.println("u2: [" + u2 + "]");
         System.out.println("u3: [" + u3 + "]");
+        System.out.println("u4: [" + u4 + "]");
+        System.out.println("u5: [" + u5 + "]");
+        System.out.println("u6: [" + u6 + "]");
+        System.out.println("u7: [" + u7 + "]");
     }
 
 }
