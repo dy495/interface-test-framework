af691e57d795a34a8b5cb1a456581cca8797dcb4
yuhaisheng <yuhaisheng@winsense.ai>
2019-07-14 13:34:15 +0800

    update: add shelf algorithm accuracy push

--- src/main/java/com/haisheng/framework/dao/IShelfDao.java
+++ src/main/java/com/haisheng/framework/dao/IShelfDao.java
@@ -4,9 +4,12 @@ package com.haisheng.framework.dao;
 import com.haisheng.framework.model.bean.Shelf;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface IShelfDao {
 
     int insert(Shelf shelf);
+    List<Shelf> query(String date);
 }
--- src/main/java/com/haisheng/framework/testng/alarm/CommodityShelfAlarm.java
+++ src/main/java/com/haisheng/framework/testng/alarm/CommodityShelfAlarm.java
@@ -1,15 +1,25 @@
 package com.haisheng.framework.testng.alarm;
 
+import com.haisheng.framework.model.bean.Shelf;
 import com.haisheng.framework.testng.CommonDataStructure.ChecklistDbInfo;
 import com.haisheng.framework.testng.CommonDataStructure.DingWebhook;
 import com.haisheng.framework.util.AlarmPush;
+import com.haisheng.framework.util.DateTimeUtil;
+import com.haisheng.framework.util.QADbUtil;
 import com.haisheng.framework.util.ServiceChecklistRun;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class CommodityShelfAlarm {
     ServiceChecklistRun checklistRun = new ServiceChecklistRun();
     int APP_ID = ChecklistDbInfo.DB_APP_ID_SHELF_SERVICE;
     int SHELF_CONF_ID = ChecklistDbInfo.DB_SERVICE_ID_SHELF_SERVICE;
+    private QADbUtil qaDbUtil = new QADbUtil();
+    private DateTimeUtil dt = new DateTimeUtil();
+
 
 
     @Test
@@ -20,10 +30,22 @@ public class CommodityShelfAlarm {
 
         String[] shelfPassRate = checklistRun.getPassRate(APP_ID, SHELF_CONF_ID);
         String[] bugInfo = checklistRun.getBugInfo(APP_ID);
+        List<Shelf> accuracyList = qaDbUtil.getShelfAccuracy(dt.getHistoryDate(0));
+
 
         AlarmPush alarmPush = new AlarmPush();
         alarmPush.setDingWebhook(DingWebhook.COMMODITY_SHELF_GRP);
-        alarmPush.shelfAlarm(shelfPassRate, bugInfo);
+        alarmPush.shelfAlarm(shelfPassRate, accuracyList, bugInfo);
 
+    }
+
+    @BeforeSuite
+    private void initial() {
+        qaDbUtil.openConnection();
+    }
+
+    @AfterSuite
+    public void clean() {
+        qaDbUtil.closeConnection();
     }
 }
