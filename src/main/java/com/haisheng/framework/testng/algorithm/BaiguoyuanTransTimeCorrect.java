477b0982ec7d3c7b756434937ce6cad465334cdd
yuhaisheng <yuhaisheng@winsense.ai>
2019-07-20 10:51:13 +0800

    update: baiguoyuan trans time correct case

--- src/main/java/com/haisheng/framework/testng/algorithm/BaiguoyuanTransTimeCorrect.java
+++ src/main/java/com/haisheng/framework/testng/algorithm/BaiguoyuanTransTimeCorrect.java
@@ -18,36 +18,71 @@ public class BaiguoyuanTransTimeCorrect {
     DateTimeUtil dt   = new DateTimeUtil();
 
     String TRANS_FILE = System.getProperty("TRANS_REPORT_FILE");
+    String TRANS_FILE_CORRECT = TRANS_FILE + ".correct";
+    String TRANS_FILE_UPLOAD = TRANS_FILE + ".upload";
     String USER_ID_KEY = System.getProperty("USER_ID_KEY");
     String CORRECT_SHIF = System.getProperty("CORRECT_SHIF"); //+00:00:03 OR -00:00:03
     String VIDEO_RECORD_BEGIN_TIME = System.getProperty("VIDEO_RECORD_BEGIN_TIME"); //10:14:41
 
     String PATTERN = "yyyy-MM-dd HH:mm:ss"; //HH: 24h, hh:12h
 
-    boolean IS_DEBUG = true;
+    boolean IS_DEBUG = false;
 
     @Test
     private void correctTransTime() throws Exception {
 
         if (IS_DEBUG) {
             TRANS_FILE = "src/main/resources/test-res-repo/baiguoyuan-metircs/timeshift.csv";
+            TRANS_FILE_CORRECT = TRANS_FILE + ".correct";
+            TRANS_FILE_UPLOAD = TRANS_FILE + ".upload";
             USER_ID_KEY = "7_17_12h";
             CORRECT_SHIF = "+00:03:00";
             VIDEO_RECORD_BEGIN_TIME = "10:14:41";
         }
 
         List<String> lines = fileUtil.getFileContent(TRANS_FILE);
+        List<String> correctLines = transferLinesToCorrect(lines);
+        saveToCorrectFile(correctLines);
+        saveToUploadFile(correctLines);
+
+
+
+    }
+
+    private List<String> transferLinesToCorrect(List<String> lines) throws Exception {
         List<String> correctLines = new ArrayList<>();
         for (String line : lines ) {
             correctLine(correctLines, line);
         }
-        lines = null;
+        return correctLines;
+    }
+
+    private boolean saveToCorrectFile(List<String> lines) {
         //write lines to trans file
-        boolean result = fileUtil.writeContentToFile(TRANS_FILE, correctLines);
-        correctLines = null;
+        boolean result = fileUtil.writeContentToFile(TRANS_FILE_CORRECT, lines);
+
         Assert.assertTrue(result, "ERROR: fail to write correct line to file");
+        logger.info("generate file " + TRANS_FILE_CORRECT + " done");
+        return result;
+    }
 
+    private boolean saveToUploadFile(List<String> lines) {
 
+        List<String> uploadList = new ArrayList<>();
+        String sep = ",";
+        //#会员编号0，日期1，时间2，纠正时间3，视频时间偏移4，交易类型
+        for (String line: lines) {
+            String[] items = line.split(",");
+            //baiguoyuan_1,00:01:26-00:01:26,1
+            String reorgLine = items[0] + sep + items[4] + "-" + items[4] + sep + "1";
+            uploadList.add(reorgLine);
+        }
+        //write lines to upload file
+        boolean result = fileUtil.writeContentToFile(TRANS_FILE_UPLOAD, uploadList);
+        Assert.assertTrue(result, "ERROR: fail to write line to upload file");
+        logger.info("generate file " + TRANS_FILE_UPLOAD + " done");
+
+        return result;
     }
 
     private void correctLine(List<String> correctLines, String line) throws Exception {
@@ -61,7 +96,7 @@ public class BaiguoyuanTransTimeCorrect {
         String sep = ",";
         int num = correctLines.size() + 1;
         //set user id
-        reorgLine += USER_ID_KEY + "_" + correctLines.size() + sep;
+        reorgLine += USER_ID_KEY + "_" + num + sep;
 
         //set date
         String date = itemArray[1].trim().replace("/", "-");
