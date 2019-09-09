a5f8d54b448d4a174c9d489f500e63d2046c0a93
yuhaisheng <yuhaisheng@winsense.ai>
2019-09-09 11:45:58 +0800

    add oss more info

--- src/main/java/com/haisheng/framework/testng/demo/TestOssClient.java
+++ src/main/java/com/haisheng/framework/testng/demo/TestOssClient.java
@@ -14,6 +14,10 @@ public class TestOssClient {
         String url = ossClientUtil.genUrl("QA_TEST/xuyan.jpg");
         System.out.println("url: " + url);
 
+        //目前OSS上测试的文件夹内只存有xuyan和yanghang两张照片
+        //QA_TEST--在oss上创建的测试使用的文件夹
+        //QA_TEST/xuyan.jpg  徐艳照片路径
+        //QA_TEST/yanghang.jpg 杨航照片路径
 
     }
 }
