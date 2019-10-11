9989c076d0fd0cacdb039a7204c61664688b2d31
yuhaisheng <yuhaisheng@winsense.ai>
2019-10-11 10:14:16 +0800

    update dingding push msg for online pvuv monitor

--- src/main/java/com/haisheng/framework/testng/custemorGateTest/online/PVUVMonitor.java
+++ src/main/java/com/haisheng/framework/testng/custemorGateTest/online/PVUVMonitor.java
@@ -59,7 +59,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataYingshiDaily() throws Exception {
+    public void getHistoryDataYingshiDaily() {
         String shopId = "8";
         String appId  = "097332a388c2";
         String com    = "赢识日常";
@@ -72,7 +72,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataYingshiOnline() throws Exception {
+    public void getHistoryDataYingshiOnline() {
         String shopId = "97";
         String appId  = "111112a388c2";
         String com    = "赢识线上";
@@ -81,7 +81,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataBaihuaOnline() throws Exception {
+    public void getHistoryDataBaihuaOnline() {
         String shopId = "149";
         String appId  = "77d07cf38e8e";
         String com    = "百花时代广场(东莞)线上";
@@ -90,7 +90,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataHumenOnline() throws Exception {
+    public void getHistoryDataHumenOnline() {
         String shopId = "251";
         String appId  = "77d07cf38e8e";
         String com    = "虎门国际购物中心线上";
@@ -99,7 +99,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataLiyingOnline() throws Exception {
+    public void getHistoryDataLiyingOnline() {
         String shopId = "373";
         String appId  = "77d07cf38e8e";
         String com    = "丽影广场线上";
@@ -108,7 +108,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataFengkeOnline() throws Exception {
+    public void getHistoryDataFengkeOnline() {
         String shopId = "242";
         String appId  = "5f20ed10b9cb";
         String com    = "万达广场丰科店线上";
@@ -117,7 +117,7 @@ public class PVUVMonitor {
     }
 
     @Test
-    public void getHistoryDataBaiguoyuanOnline() throws Exception {
+    public void getHistoryDataBaiguoyuanOnline() {
         String shopId = "246";
         String appId  = "2cf019f4c443";
         String com    = "百果园-测试店线上";
@@ -139,7 +139,7 @@ public class PVUVMonitor {
                     "\"system\":{\"app_id\":\"" + appId + "\",\"source\":\"BIZ\"}" +
                     "}";
 
-            String response = sendRequest(url, json);
+            String response = sendRequest(com, url, json);
             checkCode(response, StatusCode.SUCCESS, com + "环境监测-实时统计查询-返回值异常");
 
             saveData(response, com, date);
@@ -149,12 +149,18 @@ public class PVUVMonitor {
         }
     }
 
-    private void getHistoryDataByShop(String lb, String shopId, String appId, String com) throws Exception {
+    private void getHistoryDataByShop(String lb, String shopId, String appId, String com) {
         String router = "/business/customer/QUERY_CUSTOMER_STATISTICS/v1.1";
         String url    = lb + router;
-        String endTime   = dt.getHourBegin(0);
-        String startTime = dt.getHourBegin(-1);
-        HOUR = dt.getCurrentHour();
+        String startTime = null, endTime = null;
+
+        try {
+            startTime = dt.getHourBegin(-1);
+            endTime = dt.getHourBegin(0);
+            HOUR = dt.getCurrentHour();
+        } catch (Exception e) {
+            dingPush(com + "-巡检代码获取当前小时整点时间戳异常: " + e.toString());
+        }
 
         if (HOUR.equals("0")) {
             HOUR = "24";
@@ -168,100 +174,112 @@ public class PVUVMonitor {
             startTime = String.valueOf(dt.getHistoryDateTimestamp(-1));
             historyMonitor(url, startTime, endTime, shopId, appId, com);
         }
+
     }
 
     private void historyMonitor(String url, String startTime, String endTime, String shopId, String appId, String com) {
-        try {
-            String requestId = UUID.randomUUID().toString();
-            String date = dt.getHistoryDate(0);
+        String requestId = UUID.randomUUID().toString();
+        String date = dt.getHistoryDate(0);
 
-            if (HOUR.equals("24")) {
-                //0点后获取昨天24点的数据，数据日期为昨天
-                date = dt.getHistoryDate(-1);
-            }
-            String json = "{" +
+        if (HOUR.equals("24")) {
+            //0点后获取昨天24点的数据，数据日期为昨天
+            date = dt.getHistoryDate(-1);
+        }
+        String json = "{" +
+                "\"data\":{" +
+                "\"shop_id\":" + shopId + "," +
+                "\"start_time\":" + startTime + "," +
+                "\"end_time\":" + endTime +
+                "}," +
+                "\"request_id\":\"" + requestId + "\"," +
+                "\"system\":{\"app_id\":\"" + appId + "\",\"source\":\"BIZ\"}" +
+                "}";
+
+        if (HOUR.equals("all")) {
+            //全天历史数据统计的是昨天的流量
+            date = dt.getHistoryDate(-1);
+            json = "{" +
                     "\"data\":{" +
                     "\"shop_id\":" + shopId + "," +
                     "\"start_time\":" + startTime + "," +
-                    "\"end_time\":" + endTime +
+                    "\"end_time\":" + endTime + "," +
+                    "\"time_statistics_type\":" + "\"DAY\"" +
                     "}," +
                     "\"request_id\":\"" + requestId + "\"," +
                     "\"system\":{\"app_id\":\"" + appId + "\",\"source\":\"BIZ\"}" +
                     "}";
+        }
+        String response = sendRequest(com, url, json);
+        checkCode(response, StatusCode.SUCCESS, com + "环境监测-历史统计查询-返回值异常");
+        saveData(response, com, date);
+    }
 
-            if (HOUR.equals("all")) {
-                //全天历史数据统计的是昨天的流量
-                date = dt.getHistoryDate(-1);
-                json = "{" +
-                        "\"data\":{" +
-                        "\"shop_id\":" + shopId + "," +
-                        "\"start_time\":" + startTime + "," +
-                        "\"end_time\":" + endTime + "," +
-                        "\"time_statistics_type\":" + "\"DAY\"" +
-                        "}," +
-                        "\"request_id\":\"" + requestId + "\"," +
-                        "\"system\":{\"app_id\":\"" + appId + "\",\"source\":\"BIZ\"}" +
-                        "}";
+
+    private void saveData(String response, String com, String date) {
+
+        try {
+            OnlinePVUV onlinePVUV = new OnlinePVUV();
+            JSONObject responseJo = JSON.parseObject(response);
+            JSONArray statistics  = responseJo.getJSONArray("statistics");
+
+            String gender = null, age = null;
+            int pvEnter = 0, pvLeave = 0, uvEnter = 0, uvLeave = 0;
+
+            if (statistics != null) {
+                int size = statistics.size();
+                if (size == 0) {
+                    dingPush(com + "-历史统计查询接口-返回数据为空");
+                } else {
+                    JSONObject statisticsSingle = statistics.getJSONObject(0);
+
+                    gender = statisticsSingle.getString("gender");
+                    age = statisticsSingle.getString("age");
+
+                    String uvLeaveStr = statisticsSingle.getJSONObject("person_number").getString("entrance_leave_total");
+                    uvLeave = Integer.parseInt(uvLeaveStr);
+
+                    String uvEnterStr = statisticsSingle.getJSONObject("person_number").getString("entrance_enter_total");
+                    uvEnter = Integer.parseInt(uvEnterStr);
+
+                    String pvLeaveStr = statisticsSingle.getJSONObject("passing_times").getString("entrance_leave_total");
+                    pvLeave = Integer.parseInt(pvLeaveStr);
+
+                    String pvEnterStr = statisticsSingle.getJSONObject("passing_times").getString("entrance_enter_total");
+                    pvEnter = Integer.parseInt(pvEnterStr);
+                }
             }
-            String response = sendRequest(url, json);
-            checkCode(response, StatusCode.SUCCESS, com + "环境监测-历史统计查询-返回值异常");
-            saveData(response, com, date);
 
+            onlinePVUV.setCom(com);
+            onlinePVUV.setDate(date);
+            onlinePVUV.setHour(HOUR);
+            onlinePVUV.setGender(gender);
+            onlinePVUV.setAge(age);
+            onlinePVUV.setPvEnter(pvEnter);
+            onlinePVUV.setPvLeave(pvLeave);
+            onlinePVUV.setUvEnter(uvEnter);
+            onlinePVUV.setUvLeave(uvLeave);
+            onlinePVUV.setUpdateTime(dt.currentDateToTimestamp());
+
+            qaDbUtil.saveOnlinePvUv(onlinePVUV);
         } catch (Exception e) {
-            dingPush(com + "-巡检代码异常: " + e.toString());
-        }
-    }
-
-
-    private void saveData(String response, String com, String date) throws Exception {
-        OnlinePVUV onlinePVUV = new OnlinePVUV();
-        JSONObject responseJo = JSON.parseObject(response);
-        JSONArray statistics  = responseJo.getJSONArray("statistics");
-
-        if (statistics != null) {
-            int size = statistics.size();
-            if (size == 0) {
-                dingPush(com + "-实时统计查询接口-返回数据为空");
-            }
+            dingPush(com + "-历史统计查询接口返回response数据异常: " + e.toString() + "\n" + "response: " + response);
         }
 
-        JSONObject statisticsSingle = statistics.getJSONObject(0);
-
-        JSONObject gender = statisticsSingle.getJSONObject("gender");
-        JSONObject age = statisticsSingle.getJSONObject("age");
-
-        String uvLeaveStr = statisticsSingle.getJSONObject("person_number").getString("entrance_leave_total");
-        int uvLeave = Integer.parseInt(uvLeaveStr);
-
-        String uvEnterStr = statisticsSingle.getJSONObject("person_number").getString("entrance_enter_total");
-        int uvEnter = Integer.parseInt(uvEnterStr);
-
-        String pvLeaveStr = statisticsSingle.getJSONObject("passing_times").getString("entrance_leave_total");
-        int pvLeave = Integer.parseInt(pvLeaveStr);
-
-        String pvEnterStr = statisticsSingle.getJSONObject("passing_times").getString("entrance_enter_total");
-        int pvEnter = Integer.parseInt(pvEnterStr);
-
-        onlinePVUV.setCom(com);
-        onlinePVUV.setDate(date);
-        onlinePVUV.setHour(HOUR);
-        onlinePVUV.setGender(gender.toJSONString());
-        onlinePVUV.setAge(age.toJSONString());
-        onlinePVUV.setPvEnter(pvEnter);
-        onlinePVUV.setPvLeave(pvLeave);
-        onlinePVUV.setUvEnter(uvEnter);
-        onlinePVUV.setUvLeave(uvLeave);
-        onlinePVUV.setUpdateTime(dt.currentDateToTimestamp());
-
-        qaDbUtil.saveOnlinePvUv(onlinePVUV);
 
     }
 
-    private String sendRequest(String url, String json) throws Exception {
-        HttpExecutorUtil executorUtil = new HttpExecutorUtil();
-        executorUtil.doPostJson(url, json);
+    private String sendRequest(String com, String url, String json) {
+        try {
+            HttpExecutorUtil executorUtil = new HttpExecutorUtil();
+            executorUtil.doPostJson(url, json);
 
-        return executorUtil.getResponse();
+            return executorUtil.getResponse();
+        } catch (Exception e) {
+            dingPush(com + "-历史统计查询接口请求异常: " + e.toString());
+        }
+
+        return null;
+
     }
 
     private void checkCode(String response, int expect, String message) {
