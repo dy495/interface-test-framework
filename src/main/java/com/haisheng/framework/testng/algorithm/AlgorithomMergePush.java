7159d9b35c392aebf48ff7088e523e841acf4e92
yuhaisheng <yuhaisheng@winsense.ai>
2019-11-06 15:50:47 +0800

    add algorithm merge daily rgn

--- src/main/java/com/haisheng/framework/testng/algorithm/AlgorithomMergePush.java
+++ src/main/java/com/haisheng/framework/testng/algorithm/AlgorithomMergePush.java
@@ -1,47 +1,83 @@
 package com.haisheng.framework.testng.algorithm;
 
+import com.haisheng.framework.model.bean.OnlineAlgorithmMerge;
+import com.haisheng.framework.model.bean.OnlineScopeInfo;
+import com.haisheng.framework.testng.CommonDataStructure.ConstantVar;
+import com.haisheng.framework.testng.CommonDataStructure.DingWebhook;
+import com.haisheng.framework.util.DateTimeUtil;
+import com.haisheng.framework.util.DingChatbot;
+import com.haisheng.framework.util.QADbUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 public class AlgorithomMergePush {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private QADbUtil qaDbUtil = new QADbUtil();
+    private DateTimeUtil dt   = new DateTimeUtil();
+    private String DEBUG  = System.getProperty("DEBUG", "true");
+
+
+    @BeforeSuite
+    public void initial() {
+        qaDbUtil.openConnection();
+    }
+
+    @AfterSuite
+    public void clean() {
+        qaDbUtil.closeConnection();
+    }
 
     @Test
-    private void test() {
-        String[] atMobiles = {"1", "2"};
-        String textMsg = "{" +
-                "\"msgtype\": \"markdown\"," +
-                "\"markdown\": {\"title\":\"QA推送\"," +
-                "\"text\": \"" + "mmmmmmmmmmmmmmmmmssssssssssssssssssssgggggggggggggggg" + "\"" +
-                "}" +
-                    ",\"at\": {" +
-                                "\"atMobiles\": [" +
-                                                    strArrayToString(atMobiles) +
-                                                "], " +
-                                "\"isAtAll\": false" +
-                    "}}";
+    private void pushDailyData() {
 
-        logger.info(textMsg);
+        String today = dt.getHistoryDate(-1);
+        //get data from db
+        List<OnlineAlgorithmMerge> algorithmMergeList = qaDbUtil.selectAlgorithmMergeData(today);
+        List<OnlineScopeInfo> scopeInfoList = qaDbUtil.selectScopeInfo();
+
+        //push to daily rgn grp
+        dingPush(algorithmMergeList, scopeInfoList);
     }
 
-    private String strArrayToString(String[] strArray) {
-        String result = "";
-        int size = strArray.length;
 
-        if (1 == size) {
-            result = "\"" + strArray[0] + "\"";
-        } else {
-            //>1
-            for (int i=0; i<size; i++) {
-                if (i < size-1) {
-                    result += "\"" + strArray[i] + "\",";
-                } else {
-                    result += "\"" + strArray[i] + "\"";
-                }
-            }
+    private void dingPush(List<OnlineAlgorithmMerge> algorithmMergeList, List<OnlineScopeInfo> scopeInfoList) {
+        DingChatbot.WEBHOOK_TOKEN = DingWebhook.DAILY_PV_UV_ACCURACY_GRP;
+
+        if (DEBUG.toLowerCase().equals("true")) {
+            DingChatbot.WEBHOOK_TOKEN = DingWebhook.AD_GRP;
         }
 
-        return result;
+        String day = dt.getHistoryDate(-1);
+        String summary = "算法聚类回归";
+        String msg = "### " + summary + "\n";
+        msg += "\n\n#### " + day + " 记录信息\n";
+        String link = ConstantVar.GRAPH_DASHBORD;
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        for ( OnlineAlgorithmMerge item : algorithmMergeList) {
+            String scopeName = item.getScope();
+            for (OnlineScopeInfo scopeInfo : scopeInfoList) {
+                if (scopeInfo.getScope().equals(item.getScope())) {
+                    scopeName = scopeInfo.getScopeName();
+                    break;
+                }
+            }
+            msg += "\n>##### " + scopeName;
+            msg += "\n>###### member_percent：" + df.format(item.getMemberPercent()*100) + "%"+ "\n";
+            msg += "\n>###### avg_record：" + item.getAvgRecord() + "\n";
+        }
+        msg += "\n##### 历史信息请点击[链接](" + link +")";
+
+        logger.info("\n\n============================================\n"
+                + msg
+                + "\n============================================\n\n");
+        DingChatbot.sendMarkdown(msg);
     }
+
 }
