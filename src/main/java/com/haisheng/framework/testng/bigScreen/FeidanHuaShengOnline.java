83d59c3a1c43cd8f0ac6ab1b5d940b4f1be7c258
lvxueqing <2842726905@qq.com>
2020-03-22 14:05:36 +0800

    华盛线上 + 人脸搜索有结果状态码1000

---
+++ src/main/java/com/haisheng/framework/testng/bigScreen/FeidanHuaShengOnline.java
@@ -0,0 +1,406 @@
+package com.haisheng.framework.testng.bigScreen;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.arronlong.httpclientutil.HttpClientUtil;
+import com.arronlong.httpclientutil.builder.HCB;
+import com.arronlong.httpclientutil.common.HttpConfig;
+import com.arronlong.httpclientutil.common.HttpHeader;
+import com.arronlong.httpclientutil.exception.HttpProcessException;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.haisheng.framework.model.bean.Case;
+import com.haisheng.framework.testng.CommonDataStructure.ChecklistDbInfo;
+import com.haisheng.framework.testng.CommonDataStructure.DingWebhook;
+import com.haisheng.framework.util.*;
+import org.apache.commons.lang.text.StrSubstitutor;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @author : huachengyu
+ * @date :  2019/11/21  14:55
+ */
+
+public class FeidanHuaShengOnline {
+
+    /**
+     * 获取登录信息 如果上述初始化方法（initHttpConfig）使用的authorization 过期，请先调用此方法获取
+     *
+     * @ 异常
+     */
+    @BeforeSuite
+    public void login() {
+        qaDbUtil.openConnection();
+        String caseName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+
+        initHttpConfig();
+        String path = "/risk-login";
+        String loginUrl = getIpPort() + path;
+        String json = "{\"username\":\"huasheng@huasheng.com\",\"passwd\":\"8f5c4d276bff34502ef73f44e05fbe41\"}";
+        config.url(loginUrl)
+                .json(json);
+        logger.info("{} json param: {}", path, json);
+        long start = System.currentTimeMillis();
+        try {
+            response = HttpClientUtil.post(config);
+            this.authorization = JSONObject.parseObject(response).getJSONObject("data").getString("token");
+            logger.info("authorization: {}", this.authorization);
+        } catch (Exception e) {
+            aCase.setFailReason("http post 调用异常，url = " + loginUrl + "\n" + e);
+            logger.error(aCase.getFailReason());
+            logger.error(e.toString());
+        }
+        logger.info("{} time used {} ms", path, System.currentTimeMillis() - start);
+
+        saveData(aCase, caseName, caseName, "登录获取authentication");
+    }
+
+    @AfterSuite
+    public void clean() {
+        qaDbUtil.closeConnection();
+        dingPushFinal();
+    }
+
+    @BeforeMethod
+    public void initialVars() {
+        failReason = "";
+        response = "";
+        aCase = new Case();
+    }
+
+    @Test
+    public void testShopList() {
+        String caseName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+
+        try {
+            String path = "/risk/shop/list";
+            String json = "{}";
+            String checkColumnName = "list";
+            httpPostWithCheckCode(path, json, checkColumnName);
+
+        } catch (Exception e) {
+            failReason += e.toString();
+            aCase.setFailReason(failReason);
+
+        } finally {
+            saveData(aCase, caseName, caseName, "校验shop");
+        }
+
+    }
+
+    private Object getShopId() {
+        return "3871";
+    }
+
+
+
+//-----------------人脸搜索-----------
+
+    /**
+     * V3.0人脸搜索页面-上传png人脸图片
+     **/
+    @Test
+    public void FaceSearch_jpg(){
+        String ciCaseName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+
+        String caseName = ciCaseName;
+
+        try {
+
+            String path = "src/main/java/com/haisheng/framework/testng/bigScreen/feidanImages/华盛线上员工.png";
+            System.out.println(imageUpload(path).getJSONObject("data"));
+            JSONObject response = imageUpload(path).getJSONObject("data");
+            String face_url_tmp = response.getString("face_url_tmp");
+            String face = faceTraces(face_url_tmp);
+            JSONObject trace = JSON.parseObject(face);
+            int code = trace.getInteger("code");
+            JSONArray list = trace.getJSONObject("data").getJSONArray("list");
+
+            Preconditions.checkArgument(code==1000,"状态码不正确，期待1000，实际" + code + "\n");
+            Preconditions.checkArgument(list.size() > 0 , "无搜索结果" + "\n");
+        } catch (AssertionError e) {
+            failReason += e.toString();
+            aCase.setFailReason(failReason);
+        } catch (Exception e) {
+            failReason += e.toString();
+            aCase.setFailReason(failReason);
+        } finally {
+            saveData(aCase, ciCaseName, caseName, "校验：人脸搜索页面，上传PNG格式员工图片\n");
+        }
+    }
+
+
+
+
+//    ----------------------------------------------接口方法--------------------------------------------------------------------
+
+    /**
+     *人脸搜索上传图片
+     */
+    public JSONObject imageUpload(String path) throws Exception {
+        String url = "http://store.winsenseos.com/risk/imageUpload";
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(url);
+        httppost.addHeader("authorization", authorization);
+        httppost.addHeader("shop_id", String.valueOf(getShopId()));
+        File file = new File(path);
+        MultipartEntityBuilder mpEntity = MultipartEntityBuilder.create();
+        if (file.toString().contains("png")) {
+            mpEntity.addBinaryBody("img_file", file,ContentType.IMAGE_PNG,file.getName());
+        }
+        if (file.toString().contains("txt")) {
+            mpEntity.addBinaryBody("img_file", file,ContentType.TEXT_PLAIN,file.getName());
+        }
+        if (file.toString().contains("jpg")) {
+            mpEntity.addBinaryBody("img_file", file,ContentType.IMAGE_JPEG,file.getName());
+        }
+
+        mpEntity.addTextBody("path","undefined", ContentType.TEXT_PLAIN);
+        HttpEntity httpEntity = mpEntity.build();
+        httppost.setEntity(httpEntity);
+        System.out.println("executing request " + httppost.getRequestLine());
+        HttpResponse response = httpClient.execute(httppost);
+        HttpEntity resEntity = response.getEntity();
+        this.response = EntityUtils.toString(resEntity, "UTF-8");
+        System.out.println(response.getStatusLine());
+        checkCode(this.response, StatusCode.SUCCESS, file.getName() + "\n");
+        return JSON.parseObject(this.response);
+    }
+
+
+    /**
+     * 人脸轨迹搜索
+     */
+    public String faceTraces(String showUrl) throws Exception {
+        String url = "/risk/evidence/face/traces";
+        url = getIpPort() + url;
+        String json =
+                "{\n" +
+                        "    \"shop_id\":" + getShopId() + ",\n" +
+                        "    \"show_url\":\"" + showUrl + "\"" +
+                        "}";
+
+        String res = httpPostUrl(url, json);
+
+        return res;
+    }
+
+    private void checkCode(String response, int expect, String message) throws Exception {
+        JSONObject resJo = JSON.parseObject(response);
+
+        if (resJo.containsKey("code")) {
+            int code = resJo.getInteger("code");
+
+            message += resJo.getString("message");
+
+            if (expect != code) {
+                throw new Exception(message + " expect code: " + expect + ",actual: " + code);
+            }
+        } else {
+            int status = resJo.getInteger("status");
+            String path = resJo.getString("path");
+            throw new Exception("接口调用失败，status：" + status + ",path:" + path);
+        }
+    }
+
+//-------------------------------------------------------------用例用到的方法--------------------------------------------------------------------
+
+
+    private String httpPostUrl(String path, String json) throws Exception {
+        initHttpConfig();
+        config.url(path).json(json);
+        logger.info("{} json param: {}", path, json);
+        long start = System.currentTimeMillis();
+
+        response = HttpClientUtil.post(config);
+
+        logger.info("{} time used {} ms", path, System.currentTimeMillis() - start);
+        return response;
+    }
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private String failReason = "";
+    private String response = "";
+    private boolean FAIL = false;
+    private Case aCase = new Case();
+
+    DateTimeUtil dateTimeUtil = new DateTimeUtil();
+    CheckUtil checkUtil = new CheckUtil();
+    private QADbUtil qaDbUtil = new QADbUtil();
+    private int APP_ID = ChecklistDbInfo.DB_APP_ID_SCREEN_SERVICE;
+    private int CONFIG_ID = ChecklistDbInfo.DB_SERVICE_ID_FEIDAN_ONLINE_SERVICE;
+
+    private String CI_CMD = "curl -X POST http://qarobot:qarobot@192.168.50.2:8080/job/feidan-online-test/buildWithParameters?case_name=";
+
+    private String DEBUG = System.getProperty("DEBUG", "true");
+
+    private String authorization = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLotornp4DmtYvor5XotKblj7ciLCJ1aWQiOiJ1aWRfZWY2ZDJkZTUiLCJsb2dpblRpbWUiOjE1NzQyNDE5NDIxNjV9.lR3Emp8iFv5xMZYryi0Dzp94kmNT47hzk2uQP9DbqUU";
+
+    private HttpConfig config;
+
+    String channelId = "19";
+
+    String genderMale = "MALE";
+    String genderFemale = "FEMALE";
+
+    private final int pageSize = 50;
+
+
+    String mineChannelStr = "5";
+
+    private String getIpPort() {
+        return "http://store.winsenseos.com";
+    }
+
+    private void checkResult(String result, String... checkColumnNames) {
+        logger.info("result = {}", result);
+        JSONObject res = JSONObject.parseObject(result);
+        if (!res.getInteger("code").equals(1000)) {
+            throw new RuntimeException("result code is " + res.getInteger("code") + " not success code");
+        }
+        for (String checkColumn : checkColumnNames) {
+            Object column = res.getJSONObject("data").get(checkColumn);
+            logger.info("{} : {}", checkColumn, column);
+            if (column == null) {
+                throw new RuntimeException("result does not contains column " + checkColumn);
+            }
+        }
+    }
+
+    private void initHttpConfig() {
+        HttpClient client;
+        try {
+            client = HCB.custom()
+                    .pool(50, 10)
+                    .retry(3).build();
+        } catch (HttpProcessException e) {
+            failReason = "初始化http配置异常" + "\n" + e;
+            return;
+            //throw new RuntimeException("初始化http配置异常", e);
+        }
+        //String authorization = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwidXNlcm5hbWUiOiJ5dWV4aXUiLCJleHAiOjE1NzE0NzM1OTh9.QYK9oGRG48kdwzYlYgZIeF7H2svr3xgYDV8ghBtC-YUnLzfFpP_sDI39D2_00wiVONSelVd5qQrjtsXNxRUQ_A";
+        String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
+        Header[] headers = HttpHeader.custom().contentType("application/json; charset=utf-8")
+                .other("shop_id", getShopId().toString())
+                .userAgent(userAgent)
+                .authorization(authorization)
+                .build();
+
+        config = HttpConfig.custom()
+                .headers(headers)
+                .client(client);
+    }
+
+    private String httpPostWithCheckCode(String path, String json, String... checkColumnNames) throws Exception {
+        initHttpConfig();
+        String queryUrl = getIpPort() + path;
+        config.url(queryUrl).json(json);
+        logger.info("{} json param: {}", path, json);
+        long start = System.currentTimeMillis();
+
+        response = HttpClientUtil.post(config);
+
+        logger.info("{} time used {} ms", path, System.currentTimeMillis() - start);
+        checkResult(response, checkColumnNames);
+        return response;
+    }
+
+
+    public String getValue(JSONObject data, String key) {
+        String value = data.getString(key);
+
+        if (value == null || "".equals(value)) {
+            value = "";
+        }
+
+        return value;
+    }
+
+//    --------------------------------------------------------接口方法-------------------------------------------------------
+
+
+
+
+    private void setBasicParaToDB(Case aCase, String ciCaseName, String caseName, String caseDesc) {
+        aCase.setApplicationId(APP_ID);
+        aCase.setConfigId(CONFIG_ID);
+        aCase.setCaseName(caseName);
+        aCase.setCaseDescription(caseDesc);
+        aCase.setCiCmd(CI_CMD + ciCaseName);
+        aCase.setQaOwner("于海生");
+        aCase.setExpect("code==1000");
+        aCase.setResponse(response);
+
+        if (!StringUtils.isEmpty(failReason) || !StringUtils.isEmpty(aCase.getFailReason())) {
+            aCase.setFailReason(failReason);
+        } else {
+            aCase.setResult("PASS");
+        }
+    }
+
+    private void saveData(Case aCase, String ciCaseName, String caseName, String caseDescription) {
+        setBasicParaToDB(aCase, ciCaseName, caseName, caseDescription);
+        qaDbUtil.saveToCaseTable(aCase);
+        if (!StringUtils.isEmpty(aCase.getFailReason())) {
+            logger.error(aCase.getFailReason());
+            dingPush("飞单华盛线上 \n" + aCase.getCaseDescription() + " \n" + aCase.getFailReason());
+        }
+    }
+
+    private void dingPush(String msg) {
+        if (DEBUG.trim().toLowerCase().equals("false")) {
+            AlarmPush alarmPush = new AlarmPush();
+
+            alarmPush.setDingWebhook(DingWebhook.ONLINE_OPEN_MANAGEMENT_PLATFORM_GRP);
+
+            alarmPush.onlineRgn(msg);
+            this.FAIL = true;
+        }
+        Assert.assertNull(aCase.getFailReason());
+    }
+
+    private void dingPushFinal() {
+        if (DEBUG.trim().toLowerCase().equals("false") && FAIL) {
+            AlarmPush alarmPush = new AlarmPush();
+
+            alarmPush.setDingWebhook(DingWebhook.ONLINE_OPEN_MANAGEMENT_PLATFORM_GRP);
+
+            //15898182672 华成裕
+            //18513118484 杨航
+            //15011479599 谢志东
+            //18600872221 蔡思明
+            String[] rd = {"18513118484", //杨航
+                    "15011479599", //谢志东
+                    "15898182672"}; //华成裕
+            alarmPush.alarmToRd(rd);
+        }
+    }
+
+}
+
