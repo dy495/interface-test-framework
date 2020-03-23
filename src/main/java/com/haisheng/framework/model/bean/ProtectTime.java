adbd3acecac0cfa6ea483900c377c822b7f4d8bc
sophie <1400893423@qq.com>
2020-03-23 14:03:44 +0800

    add cases

---
+++ src/main/java/com/haisheng/framework/model/bean/ProtectTime.java
@@ -0,0 +1,20 @@
+package com.haisheng.framework.model.bean;
+
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class ReportTime {
+
+    private int shopId;
+    private int channelId;
+    private int channelStaffId;
+    private String customerName;
+    private String phone;
+    private String reportTime;
+    private String protectTime;
+    private Timestamp gmtCreate;
+
+}
