4d4a1d708c3c3f2eb93de7d2c51b03d2a75b56ba
yuhaisheng <yuhaisheng@winsense.ai>
2020-04-23 14:02:59 +0800

    update checklist run, add menjin cloud checklist run

--- src/main/java/com/haisheng/framework/testng/algorithm/ChecklistRun.java
+++ src/main/java/com/haisheng/framework/testng/algorithm/ChecklistRun.java
@@ -12,11 +12,13 @@ import org.testng.annotations.Test;
 public class ChecklistRun {
 
     ServiceChecklistRun checklistRun = new ServiceChecklistRun();
-    int APP_ID = ChecklistDbInfo.DB_APP_ID_CLOUD_SERVICE;
-    int BODY_CONF_ID = ChecklistDbInfo.DB_SERVICE_ID_BODY_SERVICE;
-    int FACE_CONF_ID = ChecklistDbInfo.DB_SERVICE_ID_FACE_SERVICE;
-    int CUST_CONF_ID = ChecklistDbInfo.DB_SERVICE_ID_CUSTOMER_DATA_SERVICE;
+    int APP_ID_AG_CLOUD   = ChecklistDbInfo.DB_APP_ID_CLOUD_SERVICE;
+    int BODY_CONF_ID      = ChecklistDbInfo.DB_SERVICE_ID_BODY_SERVICE;
+    int FACE_CONF_ID      = ChecklistDbInfo.DB_SERVICE_ID_FACE_SERVICE;
+    int CUST_CONF_ID      = ChecklistDbInfo.DB_SERVICE_ID_CUSTOMER_DATA_SERVICE;
     int MENJIN_AG_CONF_ID = ChecklistDbInfo.DB_SERVICE_ID_MENJIN_ALGORITHM_DAILY_SERVICE;
+
+    int APP_ID_BIG_SCREEN = ChecklistDbInfo.DB_APP_ID_SCREEN_SERVICE;
     int MENJIN_BE_CONF_ID = ChecklistDbInfo.DB_SERVICE_ID_MENJIN_BE_DAILY_SERVICE;
 
 
@@ -25,11 +27,11 @@ public class ChecklistRun {
         String hostPort = "http://192.168.50.2:7777";
         checklistRun.setHostPort(hostPort);
 
-        checklistRun.runChecklist(APP_ID, BODY_CONF_ID);
-        checklistRun.runChecklist(APP_ID, FACE_CONF_ID);
-        checklistRun.runChecklist(APP_ID, CUST_CONF_ID);
-        checklistRun.runChecklist(APP_ID, MENJIN_AG_CONF_ID);
-        checklistRun.runChecklist(APP_ID, MENJIN_BE_CONF_ID);
+        checklistRun.runChecklist(APP_ID_AG_CLOUD, BODY_CONF_ID);
+        checklistRun.runChecklist(APP_ID_AG_CLOUD, FACE_CONF_ID);
+        checklistRun.runChecklist(APP_ID_AG_CLOUD, CUST_CONF_ID);
+        checklistRun.runChecklist(APP_ID_AG_CLOUD, MENJIN_AG_CONF_ID);
+        checklistRun.runChecklist(APP_ID_BIG_SCREEN, MENJIN_BE_CONF_ID);
 
 
     }
