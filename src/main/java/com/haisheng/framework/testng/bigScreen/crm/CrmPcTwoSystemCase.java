a4cdb6a513f81e7bcbe9543484195006273b1571
xiamingfeng <xiamingfeng@winsense.com>
2020-07-14 21:43:54 +0800

    update

--- src/main/java/com/haisheng/framework/testng/bigScreen/crm/CrmPcTwoSystemCase.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/crm/CrmPcTwoSystemCase.java
@@ -1,4 +1,4 @@
-﻿package com.haisheng.framework.testng.bigScreen.crm;
+package com.haisheng.framework.testng.bigScreen.crm;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
