7150f9168f6ed040e8bd601b2e96e050149931d8
yuhaisheng <yuhaisheng@winsense.ai>
2020-08-03 16:50:34 +0800

    update ding token

--- src/main/java/com/haisheng/framework/testng/commonDataStructure/DingWebhook.java
+++ src/main/java/com/haisheng/framework/testng/commonDataStructure/DingWebhook.java
@@ -3,14 +3,32 @@ package com.haisheng.framework.testng.commonDataStructure;
 public class DingWebhook {
 //    public static String QA_TEST_GRP = ""; //QA测试群
 
+    //QA测试群
     public static String QA_TEST_GRP = "https://oapi.dingtalk.com/robot/send?access_token=0732a60532e16e85c37dcbbd350d461d51e5b877b6e4cd7aba498acffdf1c175"; //QA测试群
+
+    //日常回归-开放平台&独立项目
     public static String OPEN_MANAGEMENT_PLATFORM_GRP = "https://oapi.dingtalk.com/robot/send?access_token=8a9eb2c50f9b3a62bca0d9963f6a7938d7f06df6ba62889673432448ea6abb4a";
+
+    //线上巡检-开放平台&独立项目
     public static String ONLINE_OPEN_MANAGEMENT_PLATFORM_GRP = "https://oapi.dingtalk.com/robot/send?access_token=3d327456dd1da2b32ce7f30c67e2242e02305a1321cfa1744afee3ed9634dbbe";
+
+    //日常回归-货架
     public static String COMMODITY_SHELF_GRP = "https://oapi.dingtalk.com/robot/send?access_token=225c6810c977c616c9c4c112e9205b7f3f7204392c4388a05ff708cbe9e4a6ee";
+
+    //日常回归-算法聚类&PV/UV准确率
     public static String PV_UV_ACCURACY_GRP = "https://oapi.dingtalk.com/robot/send?access_token=0837493692d7a7e41f6da3fda6ed8e42f8015210b1fad450a415afbcbc7b5907";
+
+    //线上巡检-PV&UV流量监控
     public static String DAILY_PV_UV_ACCURACY_GRP = "https://oapi.dingtalk.com/robot/send?access_token=6f964c9d8415010e246613f77ec13a2983f4e7958be38f68d3a1f93bfabc5028";
+
+    //日常回归-云端算法
     public static String CLOUD_ALARM_GRP = "https://oapi.dingtalk.com/robot/send?access_token=be30edcb935927a9b07aecac5e1483413b8e5ef340ab1534dc82077aa3ea60ae";
     public static String APP_CLOUD_ALARM_GRP = "https://oapi.dingtalk.com/robot/send?access_token=9607ea62b6f638bc86fe8676c2f896c52f5d0f500562b2e1673cfb887e649433";
     public static String APP_BAIGUOYUAN_ALARM_GRP = "https://oapi.dingtalk.com/robot/send?access_token=a6af5a19fd5069d6fcc34effc80f0b777d0850b3302f0d52e0a950d5cb39d5ae";
+
+    //边缘端日常回归报警群
     public static String DAILY_EDGE = "https://oapi.dingtalk.com/robot/send?access_token=4e774f32136b1404328315a84688535a55c8e7440850339c8fccde156a23bd3b";
+
+    //日常回归-管理后台
+    public static String DAILY_MANAGEMENT_PLATFORM_GRP = "https://oapi.dingtalk.com/robot/send?access_token=5fe27703e52c3ee3e8a97bf9f6a13c6e23b63ef2abff5f5c4d91f5e915f64c10";
 }
