8f48a08da6427d4542f8501445f555526a5a8d90
Guo <1513814362@qq.com>
2020-08-07 17:52:42 +0800

    优化

--- src/main/java/com/haisheng/framework/testng/bigScreen/xundianDaily/StoreCaseV3.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/xundianDaily/StoreCaseV3.java
@@ -1,5 +1,4 @@
 package com.haisheng.framework.testng.bigScreen.xundianDaily;
-
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Preconditions;
@@ -7,25 +6,19 @@ import com.haisheng.framework.testng.commonCase.TestCaseCommon;
 import com.haisheng.framework.testng.commonCase.TestCaseStd;
 import com.haisheng.framework.testng.commonDataStructure.ChecklistDbInfo;
 import com.haisheng.framework.testng.commonDataStructure.CommonConfig;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
+import org.testng.annotations.*;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-
 
 /**
- * @author : qingqing
- * @date :  2020/07/06
+ * @author : guoliya
+ * @date :  2020/08/04
  */
-
 public class StoreCaseV3 extends TestCaseCommon implements TestCaseStd {
     StoreScenarioUtil Md = StoreScenarioUtil.getInstance();
-    long shop_id = 4116;
-    int startM = 2;
-
+    String districtCode = "";
+    String shopManager = "";
+    int page = 1;
+    int size = 10;
 
     /**
      * @description: initial test class level config, such as appid/uid/ak/dinghook/push_rd_name
@@ -36,13 +29,11 @@ public class StoreCaseV3 extends TestCaseCommon implements TestCaseStd {
         logger.debug("before classs initial");
         CommonConfig commonConfig = new CommonConfig();
 
-
         //replace checklist app id and conf id
         commonConfig.checklistAppId = ChecklistDbInfo.DB_APP_ID_SCREEN_SERVICE;
         commonConfig.checklistConfId = ChecklistDbInfo.DB_SERVICE_ID_MENDIAN_DAILY_SERVICE;
         commonConfig.checklistQaOwner = "青青";
 
-//
 //        //replace backend gateway url
 //        //commonConfig.gateway = "";
 //
@@ -117,27 +108,15 @@ public class StoreCaseV3 extends TestCaseCommon implements TestCaseStd {
 //
 //    }
 
-
-
-
-    /**
-     * @author : guoliya
-     * @date :  2020/08/04
-     */
-
     /**
      * ====================列表页排序按照上个整点计算的今日到访人次排序(人次从大到小排序)======================
      */
     @Test
-    public void storeLiistRank() {
+    public void storeListRank() {
         logger.logCaseStart(caseResult.getCaseName());
         try {
-            String districtCode = "";
             String[] shopType = {};
             String shopName = "";
-            String shopManager = "";
-            int page = 1;
-            int size = 10;
             JSONObject response = Md.patrolShopRealV3A(districtCode, shopType, shopName, shopManager, page, size);
             JSONArray storeList = response.getJSONArray("list");
             for (int i = 0; i < storeList.size() - 1; i++) {
@@ -161,41 +140,35 @@ public class StoreCaseV3 extends TestCaseCommon implements TestCaseStd {
         logger.logCaseStart(caseResult.getCaseName());
         try {
             //多选
-            String district_code = "";
             String[] shopType = new String[]{"NORMAL", "COMMUNITY"};
             String shopName = "";
-            String shopManager = "";
-            int page = 1;
-            int size = 10;
             int num = 0;
             int num1 = 0;
-            JSONArray storeList = Md.patrolShopRealV3A(district_code, shopType, shopName, shopManager, page, size).getJSONArray("list");
+            JSONArray storeList = Md.patrolShopRealV3A(districtCode, shopType, shopName, shopManager, page, size).getJSONArray("list");
             for (int i = 0; i < storeList.size(); i++) {
                 Preconditions.checkArgument(storeList.getJSONObject(i).getString("type").equals("NORMAL") || storeList.getJSONObject(i).getString("type").equals("COMMUNITY"), "筛选栏多选数据有问题");
             }
 
             //单选
             String[] shopType1 = new String[]{"NORMAL"};
-            JSONArray storeList1 = Md.patrolShopRealV3A(district_code, shopType1, shopName, shopManager, page, size).getJSONArray("list");
+            JSONArray storeList1 = Md.patrolShopRealV3A(districtCode, shopType1, shopName, shopManager, page, size).getJSONArray("list");
             for (int j = 0; j < storeList1.size(); j++) {
                 Preconditions.checkArgument(storeList1.getJSONObject(j).getString("type").equals("NORMAL"), "筛选栏单选数据有问题");
             }
 
             //全选
             String[] shopType2 = new String[]{"NORMAL", "COMMUNITY", "PLAZA", "FLAGSHIP"};
-            JSONArray storeList2 = Md.patrolShopRealV3A(district_code, shopType2, shopName, shopManager, page, size).getJSONArray("list");
+            JSONArray storeList2 = Md.patrolShopRealV3A(districtCode, shopType2, shopName, shopManager, page, size).getJSONArray("list");
             for (int m = 0; m < storeList2.size(); m++) {
                 num++;
-                System.out.println("num为  " + num);
                 Preconditions.checkArgument(storeList2.getJSONObject(m).getString("type").equals("PLAZA") || storeList2.getJSONObject(m).getString("type").equals("FLAGSHIP") || storeList2.getJSONObject(m).getString("type").equals("COMMUNITY") || storeList2.getJSONObject(m).getString("type").equals("NORMAL"), "筛选栏全选数据有问题");
             }
 
             //不选
             String[] shopType3 = new String[]{};
-            JSONArray storeList3 = Md.patrolShopRealV3A(district_code, shopType3, shopName, shopManager, page, size).getJSONArray("list");
+            JSONArray storeList3 = Md.patrolShopRealV3A(districtCode, shopType3, shopName, shopManager, page, size).getJSONArray("list");
             for (int n = 0; n < storeList3.size(); n++) {
                 num1++;
-                //System.out.println("num1为  "+num1);
                 Preconditions.checkArgument(storeList2.getJSONObject(n).getString("type").equals("PLAZA") || storeList2.getJSONObject(n).getString("type").equals("FLAGSHIP") || storeList2.getJSONObject(n).getString("type").equals("COMMUNITY") || storeList2.getJSONObject(n).getString("type").equals("NORMAL"), "筛选栏不选数据有问题");
             }
             Preconditions.checkArgument(num == num1, "全选：" + num + "不选：" + num1);
@@ -211,14 +184,10 @@ public class StoreCaseV3 extends TestCaseCommon implements TestCaseStd {
      */
     @Test
     public void fuzzySearch() {
-        String district_code = "";
         String[] shopType = {};
         String shopName = "t";
-        String shopManager = "";
-        int page = 1;
-        int size = 10;
         try {
-            JSONArray storeList = Md.patrolShopRealV3A(district_code, shopType, shopName, shopManager, page, size).getJSONArray("list");
+            JSONArray storeList = Md.patrolShopRealV3A(districtCode, shopType, shopName, shopManager, page, size).getJSONArray("list");
             for (int i = 0; i < storeList.size(); i++) {
                 String string = storeList.getJSONObject(i).getString("name");
                 if(string != null&&string.contains(shopName)) {
@@ -231,23 +200,5 @@ public class StoreCaseV3 extends TestCaseCommon implements TestCaseStd {
 
 
     }
-//    private void shopTypeMd(String[] type,String name){
-//        String district_code = "";
-//        String[] shopType = type;
-//        String shopName = name;
-//        String shopManager = "";
-//        int page = 1;
-//        int size = 10;
-//        try {
-//            JSONArray storeList = Md.patrolShopRealV3A(district_code, shopType, shopName, shopManager, page, size).getJSONArray("list");
-//            for(int i = 0; i < storeList.size(); i++){
-//
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//    }
+
 }
