2448bb5e1920bd89dc37c15b6c3011b0990fd533
yuhaisheng <yuhaisheng@winsense.ai>
2020-10-09 21:08:21 +0800

    update pvuv monitor: 扩大报警阈值

--- src/main/java/com/haisheng/framework/testng/custemorGateTest/online/PVUVMonitor.java
+++ src/main/java/com/haisheng/framework/testng/custemorGateTest/online/PVUVMonitor.java
@@ -36,20 +36,21 @@ public class PVUVMonitor {
     final String ONLINE_LB         = "http://10.0.16.17";
 
     final float HOUR_DIFF_RANGE_MAX = 1f;
-    final float HOUR_DIFF_RANGE = 0.5f;
-    final float HOUR_DIFF_RANGE_100 = 0.8f;
-    final float HOUR_DIFF_RANGE_50 = 1.6f;
-    final float HOUR_DIFF_RANGE_20 = 3.2f;
-    final float HOUR_DIFF_RANGE_10 = 5f;
-    final float DAY_DIFF_RANGE = 0.1f;
+    final float HOUR_DIFF_RANGE = 0.85f;
+    final float HOUR_DIFF_RANGE_100 = 2f;
+    final float HOUR_DIFF_RANGE_50 = 4f;
+    final float HOUR_DIFF_RANGE_20 = 8f;
+    final float HOUR_DIFF_RANGE_10 = 16f;
+    final float DAY_DIFF_RANGE = 0.3f;
 
     final String RISK_MAX = "高危险报警";
     //18210113587 于
     //15898182672 华成裕
     //18810332354 刘峤
     //18600514081 段
-    final String AT_USERS = "请 @18210113587 @15898182672 @18810332354 @18600514081 关注";
-    final String[] USER_LIST = {"18210113587", "15898182672", "18810332354", "18600514081"};
+    //15724725274 蔡彪
+    final String AT_USERS = "请 @18210113587 @15898182672 @18810332354 @18600514081 @15724725274 关注";
+    final String[] USER_LIST = {"18210113587", "15898182672", "18810332354", "18600514081", "15724725274"};
 
     String HOUR   = "all";
     boolean FAIL  = false;
@@ -701,7 +702,8 @@ public class PVUVMonitor {
             //17319042091 甘甜甜
             //18611684769 杨立新
             //15037286013 夏明凤
-            String[] rd = {"13436941018", "15084928847", "13581630214", "15898182672", "18513118484", "17319042091", "18611684769", "15037286013"};
+            //15724725274 蔡彪
+            String[] rd = {"13436941018", "15084928847", "13581630214", "15898182672", "18513118484", "17319042091", "18611684769", "15037286013", "15724725274"};
             alarmPush.alarmToRd(rd);
         }
     }
