4744f6f2cd91b2afe18ae3686de1e9831510d9d4
lvxueqing <2842726905@qq.com>
2020-10-15 21:07:21 +0800

    修改首次到访时间

--- src/main/java/com/haisheng/framework/testng/bigScreen/feidanDaily/FeidanMiniApiOrderCheckDaily.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/feidanDaily/FeidanMiniApiOrderCheckDaily.java
@@ -74,8 +74,9 @@ public class FeidanMiniApiOrderCheckDaily {
     long lianjiaReportTime = 1547014265000L;//2019-01-09 14:11:05
     long noChannelReportTime = 1547034265000L;//2019-01-09 19:44:25
 
-    long firstAppearTime = 1582684439509L;
 
+    //long firstAppearTime = 1582684439509L; //原
+    long firstAppearTime = 1583900897877L;
     String defaultRuleId = "837";
     String ahead1hRuleId = "996";
     String ahead24hRuleId = "842";
