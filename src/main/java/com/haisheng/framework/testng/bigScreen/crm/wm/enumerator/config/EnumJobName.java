90bdb1ae2ed3d5a4dddec2ef299ba7f0d5b8895c
wangmin <wangmin@winsense.ai>
2020-12-15 15:54:35 +0800

    定时测试

--- src/main/java/com/haisheng/framework/testng/bigScreen/crm/wm/enumerator/config/EnumJobName.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/crm/wm/enumerator/config/EnumJobName.java
@@ -16,7 +16,10 @@ public enum EnumJobName {
 
     CRM_ONLINE_TEST("crm-online-test"),
 
-    CRM_DAY_DATA_STORE("crm-day-data-store");
+    CRM_DAY_DATA_STORE("crm-day-data-store"),
+
+    JIAOCHEN_DAILY_TEST("jiaochen-daily-test"),
+    ;
 
     EnumJobName(String jobName) {
         this.jobName = jobName;
