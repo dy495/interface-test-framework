7bf3c6cdab9a7571cd419f16ac513526ba6e140b
wangmin <wangmin@winsense.ai>
2020-12-24 12:04:52 +0800

    token修改

--- src/main/java/com/haisheng/framework/dao/IPorscheDeliverDao.java
+++ src/main/java/com/haisheng/framework/dao/IPorscheDeliverDao.java
@@ -5,7 +5,7 @@ import com.haisheng.framework.testng.bigScreen.crm.wm.bean.TPorscheDeliverInfo;
 
 import java.util.List;
 
-public interface IPorscheDao {
+public interface IPorscheDeliverDao {
 
     List<PorscheDeliverInfo> selectPorscheDeliver();
 }
--- src/main/java/com/haisheng/framework/testng/bigScreen/crm/wm/util/MyBatisUtil.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/crm/wm/util/MyBatisUtil.java
@@ -1,9 +1,7 @@
 package com.haisheng.framework.testng.bigScreen.crm.wm.util;
 
-import com.haisheng.framework.dao.IPorscheDao;
+import com.haisheng.framework.dao.IPorscheDeliverDao;
 import com.haisheng.framework.model.bean.PorscheDeliverInfo;
-import com.haisheng.framework.testng.bigScreen.crm.wm.bean.TPorscheDeliverInfo;
-import com.haisheng.framework.testng.bigScreen.feidanOnline.Link;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,10 +10,8 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class MyBatisUtil {
     static SqlSessionFactory sqlSessionFactory = null;
@@ -44,8 +40,8 @@ public class MyBatisUtil {
 
     public PorscheDeliverInfo[] ss() {
         SqlSession sqlSession = getSqlSession();
-        IPorscheDao iPorscheDao = sqlSession.getMapper(IPorscheDao.class);
-        List<PorscheDeliverInfo> porscheDeliverInfos = iPorscheDao.selectPorscheDeliver();
+        IPorscheDeliverDao iPorscheDeliverDao = sqlSession.getMapper(IPorscheDeliverDao.class);
+        List<PorscheDeliverInfo> porscheDeliverInfos = iPorscheDeliverDao.selectPorscheDeliver();
         List<PorscheDeliverInfo> newList = new LinkedList<>(porscheDeliverInfos);
         return newList.toArray(new PorscheDeliverInfo[porscheDeliverInfos.size()]);
     }
