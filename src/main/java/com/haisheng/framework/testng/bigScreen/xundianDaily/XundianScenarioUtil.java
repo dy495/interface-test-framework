060f9a41a5aabe9ca7f94836e0d2c563f235069d
qingqing0214 <317079740@qq.com>
2020-12-26 17:28:42 +0800

    add xundian case

--- src/main/java/com/haisheng/framework/testng/bigScreen/xundianDaily/XundianScenarioUtil.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/xundianDaily/XundianScenarioUtil.java
@@ -276,7 +276,7 @@ public class XundianScenarioUtil extends TestCaseCommon {
    5.5 复检、不合格处理步骤提交
    */
     public JSONObject MstepSumit(Long shop_id, Long id, String comment, JSONArray pic_list, Integer recheck_result) throws Exception {
-        String url = "/patrol/m/task/step/submit";
+        String url = "/store/m-app/auth/task/step/submit";
         String json =
                 "{" +
                         "\"shop_id\" :" + shop_id + ",\n" +
@@ -512,58 +512,6 @@ public class XundianScenarioUtil extends TestCaseCommon {
     }
 
 
-    /**
-     * 巡检员列表
-     *
-     * @return
-     * @throws Exception
-     */
-    public JSONObject inspectorList() throws Exception {
-        String url = "/patrol/schedule-check/inspector/list";
-        JSONObject json = new JSONObject();
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        System.out.println(res);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    /**
-     * 巡检门店
-     */
-    public JSONObject shopList(String inspectorId, String districtCode) throws Exception {
-        String url = "/patrol/schedule-check/shop/list";
-        String json =
-                "{\n" +
-                        "    \"inspector_id\":\"" + inspectorId + "\",\n" +
-                        "    \"district_code\":\"" + districtCode + "\"\n" +
-                        "}";
-
-        String res = httpPostWithCheckCode(url, json, IpPort);
-
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-
-    public void applogin(String userName, String passwd) {
-
-        initHttpConfig();
-        String path = "/m/patrol-login";
-        String loginUrl = IpPort + path;
-        String json = "{\"type\":0, \"username\":\"" + userName + "\",\"password\":\"" + passwd + "\"}";
-        config.url(loginUrl)
-                .json(json);
-        logger.info("{} json param: {}", path, json);
-        long start = System.currentTimeMillis();
-        try {
-            response = HttpClientUtil.post(config);
-            authorization = JSONObject.parseObject(response).getJSONObject("data").getString("token");
-            logger.info("authorization:" + authorization);
-        } catch (Exception e) {
-            appendFailReason(e.toString());
-        }
-        logger.info("{} time used {} ms", path, System.currentTimeMillis() - start);
-
-        //saveData("登陆");
-    }
 
     /**
      * @description :新建定检任务
@@ -590,32 +538,6 @@ public class XundianScenarioUtil extends TestCaseCommon {
     }
 
 
-    /**
-     * @description :获取定检任务列表
-     * @date :2020/6/21 9:59
-     **/
-    public JSONObject scheduleCheckList(int size, int page) throws Exception {
-        String url = "/patrol/schedule-check/page";
-
-        JSONObject json = new JSONObject();
-        json.put("size", size);
-        json.put("page", page);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-//    /**
-//     * @description:  删除定检任务
-//     */
-//    public JSONObject scheduleCheckDelete(long id) throws Exception {
-//        String url = "/patrol/schedule-check/delete";
-//        JSONObject json=new JSONObject();
-//        json.put("id",id);
-//        String res = httpPostWithCheckCode(url, json.toJSONString(),IpPort);
-//
-//        return JSON.parseObject(res).getJSONObject("data");
-//    }
 
     /**
      * 不合格提交图片
@@ -732,19 +654,6 @@ public class XundianScenarioUtil extends TestCaseCommon {
     }
 
     /**
-     * 寻巡店中心门店设备查询
-     */
-    public JSONObject shopDevice() throws Exception {
-        String url = "/patrol/shop/device/list";
-        String json = "{\n" +
-                "\"shop_id\":" + getXunDianShop() + "\n" +
-                "}";
-        String res = httpPostWithCheckCode(url, json, IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-
-    /**
      * shop detail
      */
     public JSONObject xunDianCenterDetail() throws Exception {
@@ -803,18 +712,6 @@ public class XundianScenarioUtil extends TestCaseCommon {
         return JSON.parseObject(res).getJSONObject("data");
     }
 
-    //巡店结果+巡店者
-    public JSONObject xundianDetil(int check_result, int page, int size, String inspector_id) throws Exception {
-        String url = "/patrol/shop/checks/page";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", getXunDianShop());
-        json.put("check_result", check_result);
-        json.put("page", page);
-        json.put("size", size);
-        json.put("inspector_id", inspector_id);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
 
     //巡店结果+处理状态
     public JSONObject xundianDetil(int check_result, int page, int size, int handle_status) throws Exception {
@@ -905,258 +802,6 @@ public class XundianScenarioUtil extends TestCaseCommon {
         String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
         return JSON.parseObject(res).getJSONObject("data");
     }
-
-    /**
-     * ----------------app 相关接口-----------------
-     *
-     * @description :代办事项列表
-     * @date :2020/6/26 20:27
-     **/
-    public JSONObject Task_list(Integer type, Integer size, Long last_id) throws Exception {
-        String url = "/patrol/m/task/list";
-        JSONObject json = new JSONObject();
-        json.put("type", type);
-        json.put("size", size);
-        json.put("last_id", last_id);
-        String res = httpPostWithCheckCode(url, json.toString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    public JSONObject Task_list(Integer type, Integer size) throws Exception {
-        String url = "/patrol/m/task/list";
-        JSONObject json = new JSONObject();
-        json.put("type", type);
-        json.put("size", size);
-        String res = httpPostWithCheckCode(url, json.toString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-
-    //app开始巡店,适用于定检任务巡店
-    public JSONObject checkStartapp(Long shop_id, String check_type, Integer reset, Long task_id) throws Exception {
-        String url = "/store/m-app/auth/shop/checks/start";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("check_type", check_type);
-        json.put("reset", reset);
-        json.put("task_id", task_id);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //适用于现场巡店
-    public JSONObject checkStartapp(Long shop_id, String check_type, Integer reset) throws Exception {
-        String url = "/store/m-app/auth/shop/checks/start";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("check_type", check_type);
-        json.put("reset", reset);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //获取门店设备
-    public JSONObject shopDevice(Long shop_id) throws Exception {
-        String url = "/patrol/m/shop/device/list";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //定检任务 定时拍照图片
-    public JSONObject picList(Long shop_id, String device_id, String date) throws Exception {
-        String url = "/patrol/m/task/schedule-pic/list";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("device_id", device_id);
-        json.put("date", date);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    /**
-     * @description :复检不合格提交
-     * @date :2020/6/26 20:34
-     **/
-
-    public JSONObject StepSubmit(Long id, String comment, Integer recheck_result, List<String> pic_list) throws Exception {
-        String url = " /patrol/m/task/step/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", getXunDianShop());
-        json.put("id", id);
-        json.put("comment", comment);
-        json.put("pic_list", pic_list);
-        json.put("recheck_result", recheck_result);
-        String res = httpPostWithCheckCode(url, json.toString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //提交执行定检 任务 巡店/远程巡店不合格处理
-    public JSONObject stepSubmit(Long shop_id, Long id, String comment) throws Exception {
-        String url = "/patrol/m/task/step/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("id", id);
-        json.put("comment", comment);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    public JSONObject stepSubmit(Long shop_id, Long id, String comment, List<String> pic_list) throws Exception {
-        String url = "/patrol/m/task/step/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("id", id);
-        json.put("comment", comment);
-        json.put("pic_list", pic_list);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //处理待办事项,提交返回code  TODO:
-    public Long stepSubmitX(Long shop_id, Long id, String comment, List<String> pic_list) throws Exception {
-        String url = "/patrol/m/task/step/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("id", id);
-        json.put("comment", comment);
-        json.put("pic_list", pic_list);
-        String res = httpPost(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getLong("code");
-    }
-
-
-    //巡检员查看处理结果 合格与不合格
-    public JSONObject stepSubmit2(Long shop_id, Long id, String comment, Integer recheck_result) throws Exception {
-        String url = "/patrol/m/task/step/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("id", id);
-        json.put("comment", comment);
-        json.put("recheck_result", recheck_result);
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //巡检员查看处理结果 合格与不合格 返回code
-    public Long stepSubmitCode(Long shop_id, Long id, String comment, Integer recheck_result) throws Exception {
-        String url = "/patrol/m/task/step/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("id", id);
-        json.put("comment", comment);
-        json.put("recheck_result", recheck_result);
-        String res = httpPost(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getLong("code");
-    }
-
-    //门店详情
-    public JSONObject taskDetail() throws Exception {
-        String url = "/patrol/m/task/detail";
-        JSONObject json = new JSONObject();
-        String res = httpPostWithCheckCode(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //app定检任务提交单个审核项，不加评论
-    public JSONObject appsubmit(Long shop_id, Integer check_result, long item_id, long list_id, long patrol_id) throws Exception {
-        String url = "/patrol/m/shop/checks/item/submit";
-        JSONObject json = new JSONObject();
-//        String shopid=getXunDianShop();
-        json.put("shop_id", shop_id);
-        json.put("check_result", check_result);
-        json.put("item_id", item_id);
-        json.put("list_id", list_id);
-        json.put("patrol_id", patrol_id);
-
-        String res = httpPost(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    //app定检任务提交单个审核项，加评论
-    public JSONObject appsubmit(Long shop_id, Integer check_result, long item_id, long list_id, long patrol_id, String comment) throws Exception {
-        String url = "/patrol/m/shop/checks/item/submit";
-        JSONObject json = new JSONObject();
-//        String shopid=getXunDianShop();
-        json.put("shop_id", shop_id);
-        json.put("check_result", check_result);
-        json.put("item_id", item_id);
-        json.put("list_id", list_id);
-        json.put("patrol_id", patrol_id);
-        json.put("audit_comment", comment);
-
-        String res = httpPost(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    /**
-     * app定检任务不合格提交图片
-     */
-    public JSONObject appSubmitN(Long shop_id, long patrolId, long listId, long itemId, List<String> picList) throws Exception {
-        String url = "/patrol/m/shop/checks/item/submit";
-
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("patrol_id", patrolId);
-        json.put("list_id", listId);
-        json.put("item_id", itemId);
-        json.put("pic_list", picList);
-        String res = httpPost(url, json.toJSONString(), IpPort);
-
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
-    /**
-     * app定检任务不合格提交图片返回code
-     */
-    public Long appSubmitNCode(Long shop_id, long patrolId, long listId, long itemId, List<String> picList) throws Exception {
-        String url = "/patrol/m/shop/checks/item/submit";
-
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("patrol_id", patrolId);
-        json.put("list_id", listId);
-        json.put("item_id", itemId);
-        json.put("pic_list", picList);
-        String res = httpPost(url, json.toJSONString(), IpPort);
-
-        return JSON.parseObject(res).getLong("code");
-    }
-
-    /**
-     * @description :app现场巡店 提交不合格图片提交返回code
-     * @date :2020/6/22 20:54
-     **/
-
-    public Integer appchecksItemSubmitY(Long shop_id, long patrolId, long listId, long itemId, List<String> picList) throws Exception {
-        String url = "/patrol/m/shop/checks/item/submit";
-
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("patrol_id", patrolId);
-        json.put("list_id", listId);
-        json.put("item_id", itemId);
-        json.put("pic_list", picList);
-
-        String res = httpPost(url, json.toJSONString(), IpPort);
-
-        return JSON.parseObject(res).getInteger("code");
-    }
-
-    /**
-     * app checks submit 定检任务单项审核之后，总提交
-     */
-    public JSONObject appcheckSubmit(Long shop_id, String commit, Long id) throws Exception {
-        String url = "/patrol/m/shop/checks/submit";
-        JSONObject json = new JSONObject();
-        json.put("shop_id", shop_id);
-        json.put("comment", commit);
-        json.put("id", id);
-        String res = httpPost(url, json.toJSONString(), IpPort);
-        return JSON.parseObject(res).getJSONObject("data");
-    }
-
 
 
     /**
@@ -1402,7 +1047,7 @@ public class XundianScenarioUtil extends TestCaseCommon {
      * @time:
      */
     public JSONObject device_replay(String device_id,long shop_id,String date,String time ) throws Exception {
-        String url = "/store/m-app/shop/device/replay";
+        String url = "/store/m-app/auth/shop/device/replay";
 
         JSONObject json = new JSONObject();
         json.put("device_id", device_id);
@@ -1452,7 +1097,7 @@ public class XundianScenarioUtil extends TestCaseCommon {
      * @time:
      */
     public JSONObject task_step_submit(Long shop_id,Long id,JSONArray pic_list,Integer recheck_result,String comment) throws Exception {
-        String url = "/store/m-app/task/step/submit";
+        String url = "/store/m-app/auth/task/step/submit";
         String json =
                 "{" +
                         "\"id\" :" + id + "\n" +
