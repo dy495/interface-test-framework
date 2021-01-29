6258b2a96844321314b2695b250fa4b1e2f4a41a
wangmin <wangmin@winsense.ai>
2021-01-29 19:57:13 +0800

    新增用例,修改接口

---
+++ src/main/java/com/haisheng/framework/testng/bigScreen/jiaochen/wm/testcase/AbnormalManager.java
@@ -0,0 +1,66 @@
+package com.haisheng.framework.testng.bigScreen.jiaochen.wm.testcase;
+
+import com.haisheng.framework.testng.bigScreen.crm.wm.base.agency.Visitor;
+import com.haisheng.framework.testng.bigScreen.crm.wm.enumerator.config.*;
+import com.haisheng.framework.testng.bigScreen.jiaochen.wm.enumerator.EnumAccount;
+import com.haisheng.framework.testng.bigScreen.jiaochen.wm.util.SupporterUtil;
+import com.haisheng.framework.testng.bigScreen.jiaochen.wm.util.UserUtil;
+import com.haisheng.framework.testng.commonCase.TestCaseCommon;
+import com.haisheng.framework.testng.commonCase.TestCaseStd;
+import com.haisheng.framework.testng.commonDataStructure.CommonConfig;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author wangmin
+ * @date 2021/1/29 10:48
+ */
+public class AbnormalManager extends TestCaseCommon implements TestCaseStd {
+    private static final EnumTestProduce product = EnumTestProduce.JIAOCHEN_ONLINE;
+    private static final EnumAccount ADMINISTRATOR = EnumAccount.ADMINISTRATOR_ONLINE;
+    private static final Integer SIZE = 100;
+    public Visitor visitor = new Visitor(product);
+    public SupporterUtil util = new SupporterUtil(visitor);
+    public UserUtil user = new UserUtil(visitor);
+
+
+    @BeforeClass
+    @Override
+    public void initial() {
+        logger.debug("before class initial");
+        CommonConfig commonConfig = new CommonConfig();
+        //替换checklist的相关信息
+        commonConfig.checklistAppId = EnumChecklistAppId.DB_APP_ID_SCREEN_SERVICE.getId();
+        commonConfig.checklistConfId = EnumChecklistConfId.DB_SERVICE_ID_CRM_DAILY_SERVICE.getId();
+        commonConfig.checklistQaOwner = EnumChecklistUser.WM.getName();
+        commonConfig.product = product.getAbbreviation();
+        commonConfig.referer = product.getReferer();
+        //替换jenkins-job的相关信息
+        commonConfig.checklistCiCmd = commonConfig.checklistCiCmd.replace(commonConfig.JOB_NAME, EnumJobName.JIAOCHEN_DAILY_TEST.getJobName());
+        commonConfig.message = commonConfig.message.replace(commonConfig.TEST_PRODUCT, product.getDesc() + commonConfig.checklistQaOwner);
+        //替换钉钉推送
+        commonConfig.dingHook = EnumDingTalkWebHook.CAR_OPEN_MANAGEMENT_PLATFORM_GRP.getWebHook();
+        //放入shopId
+        commonConfig.shopId = product.getShopId();
+        beforeClassInit(commonConfig);
+    }
+
+    @AfterClass
+    @Override
+    public void clean() {
+        afterClassClean();
+    }
+
+    @BeforeMethod
+    @Override
+    public void createFreshCase(Method method) {
+        user.loginPc(ADMINISTRATOR);
+        logger.debug("beforeMethod");
+        caseResult = getFreshCaseResult(method);
+        logger.debug("case: " + caseResult);
+    }
+
+}
