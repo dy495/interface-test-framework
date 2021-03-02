074fb4c09704ac364d2be8d4e59a193f372331e9
wangmin <wangmin@winsense.ai>
2021-03-02 18:53:11 +0800

    1.增加积分中心用例

---
+++ src/main/java/com/haisheng/framework/testng/bigScreen/jiaochen/wm/sense/applet/granted/AppletCommodityListScene.java
@@ -0,0 +1,32 @@
+package com.haisheng.framework.testng.bigScreen.jiaochen.wm.sense.applet.granted;
+
+import com.alibaba.fastjson.JSONObject;
+import com.haisheng.framework.testng.bigScreen.crm.wm.base.scene.BaseScene;
+import lombok.Builder;
+
+/**
+ * 小程序积分商城
+ */
+@Builder
+public class AppletCommodityListScene extends BaseScene {
+    private final Integer size;
+    private final Integer lastValue;
+    private final String integralSort;
+    private final Boolean status;
+
+    @Override
+    public JSONObject getRequestBody() {
+        JSONObject object = new JSONObject();
+        object.put("size", size);
+        object.put("last_value", lastValue);
+        object.put("integral_sort", integralSort);
+        object.put("status", status);
+        return object;
+    }
+
+    @Override
+    public String getPath() {
+        return "/jiaochen/applet/granted/integral-mall/commodity-list";
+    }
+
+}
