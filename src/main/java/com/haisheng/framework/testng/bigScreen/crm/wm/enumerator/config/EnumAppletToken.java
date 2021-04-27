01f716b161c8d4cace4dd05d224d4883800da34f
ztzzx <1624143131@qq.com>
2021-04-27 18:28:18 +0800

    token更新

--- src/main/java/com/haisheng/framework/testng/bigScreen/crm/wm/enumerator/config/EnumAppletToken.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/crm/wm/enumerator/config/EnumAppletToken.java
@@ -39,7 +39,7 @@ public enum EnumAppletToken {
 
     JC_GLY_ONLINE(EnumTestProduce.JC_DAILY.getAbbreviation(), "92FWKP9z1yNf7P3OLC6KPw==", false, "13373166806"),
 
-    INS_WM_DAILY(EnumTestProduce.INS_DAILY.getAbbreviation(), "KmN5WYK4ptRf5ZFL/y3cOQ==", true, "15321527989"),
+    INS_WM_DAILY(EnumTestProduce.INS_DAILY.getAbbreviation(), "PjOOJABTSaNjV98WCFWWeQ==", true, "15321527989"),
 
     INS_WM_ONLINE(EnumTestProduce.INS_ONLINE.getAbbreviation(), "3/xeCPoURoHfylTkD/d54Q==", false, "15321527989"),
 
