33c59d34fb3c9f79bf5e2fffa9cb88a9af2a2de5
wangmin <wangmin@winsense.ai>
2021-05-14 16:45:52 +0800

    去掉description注解

--- src/main/java/com/haisheng/framework/testng/bigScreen/xundianDaily/wm/bean/RealTimeShopPvUvBean.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/xundianDaily/wm/bean/RealTimeShopPvUvBean.java
@@ -1,7 +1,6 @@
 package com.haisheng.framework.testng.bigScreen.xundianDaily.wm.bean;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import com.sun.org.glassfish.gmbal.Description;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,15 +10,21 @@ import java.io.Serializable;
  */
 @Data
 public class RealTimeShopPvUvBean implements Serializable {
-    @Description("时间段")
+    /**
+     * 时间段
+     */
     @JSONField(name = "time")
     private String time;
 
-    @Description("今日人次")
+    /**
+     * 今日人次
+     */
     @JSONField(name = "today_pv")
     private Integer todayPv;
 
-    @Description("今日人数")
+    /**
+     * 今日人数
+     */
     @JSONField(name = "today_uv")
     private Integer todayUv;
 
