d3a91bc0ec849c838db6e2181bf88df2eef2af45
wangmin <wangmin@winsense.ai>
2021-08-09 19:23:57 +0800

    调试

--- src/main/java/com/haisheng/framework/testng/bigScreen/itemCms/J.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/itemCms/J.java
@@ -1,7 +1,10 @@
 package com.haisheng.framework.testng.bigScreen.itemCms;
 
+import org.testng.annotations.Test;
+
 public class J {
-    public static void main(String[] args) {
+    @Test
+    public void a() {
         String userDir = System.getProperty("user.dir");
         System.err.println(userDir);
     }
