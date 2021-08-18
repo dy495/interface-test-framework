e11dd6f5e1ae65b6f7ee4cc26585288e0376c109
ztzzx <1624143131@qq.com>
2021-08-18 12:24:59 +0800

    更改字段名

--- src/main/java/com/haisheng/framework/testng/bigScreen/itemCms/common/util/CmsConstants.java
+++ src/main/java/com/haisheng/framework/testng/bigScreen/itemCms/common/util/CmsConstants.java
@@ -11,9 +11,9 @@ public class CmsConstants {
     public static final String SHOP_TABLE_NAME = "店铺";
     public static final String ENTRANCE_TABLE_NAME = "出入口";
     public static final String TABLE_FIELD_SHOP_NAME = "店铺名";
-    public static final String TABLE_FIELD_FLOOR_NAME = "楼层";
-    public static final String TABLE_FIELD_POINT_NAME = "点位名称";
+    public static final String TABLE_FIELD_FLOOR_NAME = "*楼层";
+    public static final String TABLE_FIELD_POINT_NAME = "*点位名称";
     public static final String TABLE_FIELD_POINT_ONE = "点位1";
     public static final String TABLE_FIELD_POINT_TWO = "点位2";
-    public static final String TABLE_FIELD_ENTRANCE_TYPE = "出入口类型";
+    public static final String TABLE_FIELD_ENTRANCE_TYPE = "*出入口类型";
 }
