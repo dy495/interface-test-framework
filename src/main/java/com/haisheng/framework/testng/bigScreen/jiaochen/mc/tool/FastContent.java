5797d6c359df7cf5afeabf25376309a9793c69b4
mengchen <mengchen@winsense.ai>
2021-08-25 17:50:37 +0800

    修改

---
+++ src/main/java/com/haisheng/framework/testng/bigScreen/jiaochen/mc/tool/FastContent.java
@@ -0,0 +1,14 @@
+package com.haisheng.framework.testng.bigScreen.jiaochen.mc.tool;
+
+public class FastContent {
+    public static final String NAME51 = "赵钱孙李周吴郑王冯陈褚卫蒋沈韩杨朱秦尤许何吕施张孔曹严华金魏陶姜戚谢邹喻柏水窦章云苏潘葛奚范彭郎鲁韦昌";
+    public static final String NAME50 = "赵钱孙李周吴郑王冯陈褚卫蒋沈韩杨朱秦尤许何吕施张孔曹严华金魏陶姜戚谢邹喻柏水窦?41aB[&(_*>";
+
+
+
+
+
+
+
+
+}
